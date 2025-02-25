af442272169fcf7c85bc5c7d6c25cf45a5ca0a25
shawn <shawnstartup2023@gmail.com>
2025-02-25 21:47:49 +0800

    Remove unused code

--- command/common.go
+++ command/common.go
@@ -7,16 +7,10 @@ import (
 const (
 	NoDiscoverFlag = "no-discover"
 	BootnodeFlag   = "bootnode"
+	RelaynodeFlag  = "relaynode"
 	LogLevelFlag   = "log-level"
 )
 
 var (
-	errInvalidValidatorRange = errors.New("minimum number of validators can not be greater than the " +
-		"maximum number of validators")
-	errInvalidMinNumValidators = errors.New("minimum number of validators must be greater than 0")
-	errInvalidMaxNumValidators = errors.New("maximum number of validators must be lower or equal " +
-		"than MaxSafeJSInt (2^53 - 2)")
-
-	ErrValidatorNumberExceedsMax = errors.New("validator number exceeds max validator number")
-	ErrECDSAKeyNotFound          = errors.New("ECDSA key not found in given path")
+	errInvalidMinNumRelays = errors.New("minimum number of relay node must be greater than 0")
 )
